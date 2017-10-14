6e1c617b9fce9cdbb6611dcd8ed2188f912d2a8f
Saurabh <saurabhmimshra@gmail.com>
2017-10-14 21:53:39 +0530

    insert Scroll Reveal in home page

--- footer/man_footer.js
+++ footer/man_footer.js
@@ -1,3 +1,2 @@
-window.sr = ScrollReveal();
 sr.reveal('.footer-heading', {duration : 1000, origin : 'left'});
 sr.reveal('.social', {duration : 1000, origin : 'right' });
---
+++ home/man_index_home.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal();
+	sr.reveal(".main-image", {duration : 2000, origin : 'right', distance : '200px'});
+	sr.reveal(".odd", {duration : 1500, origin : 'left', distance : '500px'});
+	sr.reveal(".main-heading", {duration : 2000, origin : 'left', distance : '200px'});
+	sr.reveal(".even", {duration : 1500, origin : 'right', distance: '500px'});
+	sr.reveal(".activities", {duration : 1500, origin : 'bottom', distance : '500px'});
+	sr.reveal(".contact-spi", {duration : 1500, origin : 'bottom', distance : '200px'});
+	sr.reveal(".project-for-scrvl", {duration : 1500, origin : 'bottom', distance : '100px'});
