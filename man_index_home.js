86654e5bf94ee2a1bb58aa6578826604899b95cd
Saurabh <saurabhmimshra@gmail.com>
2017-10-15 09:47:55 +0530

    slowdown scroll speed

--- man_index_home.js
+++ man_index_home.js
@@ -17,7 +17,7 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	header: ['.navbar'],  // Selector for fixed headers (must be a valid CSS selector)
 
 	// Speed & Easing
-	speed: 500, // Integer. How fast to complete the scroll in milliseconds
+	speed: 800, // Integer. How fast to complete the scroll in milliseconds
 	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
 	easing: 'easeInOutCubic', // Easing pattern to use
 	customEasing: function (time) {}, // Function. Custom easing pattern
