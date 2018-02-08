d2cd4fd0d9cf12731783220a5240a1774c66f685
Saurabh <saurabhmimshra@gmail.com>
2018-02-08 16:45:13 +0530

    make module and migration

--- teamspi/home/models.py
+++ teamspi/home/models.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Member(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+class Project(models.Model):
+	title = models.CharField(max_length=500)
+	description = models.CharField(max_length= 1000)
