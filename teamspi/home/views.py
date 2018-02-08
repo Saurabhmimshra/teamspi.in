5c94fe8151cc9a1f4a22f13a2574be2a3adf63eb
Saurabh <saurabhmimshra@gmail.com>
2018-02-08 16:59:51 +0530

    changed template projects are showing

--- teamspi/home/views.py
+++ teamspi/home/views.py
@@ -2,8 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from home.models import Team, Member, Project
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	context = 	{ 
+					'first_project' : Project.objects.get(title = "Sell My Books"), 
+					'second_project' : Project.objects.get(title = 'Noteshub'),
+		 		}	
+	return render(request, 'index.html', context)
